5cba2997b7a849018e9423c4907d5981ab225fb4
Bruno Almeida <brunosalmeida@outlook.com>
2020-06-10 00:34:17 +0100

    Add FluentValidation and create UserValidation
    Add FluentValidation
    Create class UserValidator and unit test.

---
+++ src/backend/FS.Domain.Model/Validators/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace FS.Domain.Model.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Name).NotNull().WithMessage("User's name can not be null");
+            RuleFor(u => u.Name).NotEmpty().WithMessage("User's name can not be empty");
+            RuleFor(u => u.Name).MaximumLength(100).WithMessage("User's name must have less than 100 characters");
+            RuleFor(u => u.Name).MinimumLength(3).WithMessage("User's name must have more than 2 characters");
+
+            RuleFor(u => u.Email).NotNull().WithMessage("User's email can not be null");
+            RuleFor(u => u.Email).NotEmpty().WithMessage("User's email can not be empty");
+            RuleFor(u => u.Email).EmailAddress().WithMessage("User's email must have a valid format");
+            RuleFor(u => u.Email).MaximumLength(50).WithMessage("User's email must have less than 50 characters");
+            RuleFor(u => u.Email).MinimumLength(10).WithMessage("User's email must have more than 10 characters");
+
+            RuleFor(u => u.Password).NotNull().WithMessage("User's password can not be null");
+            RuleFor(u => u.Password).NotEmpty().WithMessage("User's password can not be empty");
+            RuleFor(u => u.Password).MaximumLength(20).WithMessage("User's password must have less than 20 characters");
+            RuleFor(u => u.Password).MinimumLength(6).WithMessage("User's password must have more than 6 characters");
+        }
+    }
+}
---
+++ test/FS.Domain.Model.Test/Validators/UserValidatorTest.cs
@@ -0,0 +1,127 @@
+﻿using FS.Domain.Model.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FS.Domain.Model.Test.Validators
+{
+    [TestClass]
+    public class UserValidatorTest
+    {
+        [TestMethod]
+        public void CreateUSerWithValidData_ShouldReturnsNewUserInstance()
+        {
+            var user = new User("TestName", "email@email.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        #region Property name
+
+        [TestMethod]
+        public void CreateUSerWithNameLenghtGreaterThan100_ShouldReturnsError()
+        {
+            var user = new User(new string('C', 101), "email@email.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 1);
+            Assert.AreEqual("User's name must have less than 100 characters", result.Errors.First().ErrorMessage);
+            Assert.AreEqual("MaximumLengthValidator", result.Errors.First().ErrorCode);
+        }
+
+        [TestMethod]
+        public void CreateUSerWithNameLenghtLessThan2_ShouldReturnsError()
+        {
+            var user = new User(new string('C', 2), "email@email.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 1);
+            Assert.AreEqual("User's name must have more than 2 characters", result.Errors.First().ErrorMessage);
+            Assert.AreEqual("MinimumLengthValidator", result.Errors.First().ErrorCode);
+        }
+
+        [TestMethod]
+        public void CreateUSerWithoutName_ShouldReturnsError()
+        {
+            var user = new User(null, "email@email.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 2);
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorMessage == "User's name can not be null"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorMessage == "User's name can not be empty"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorCode == "NotNullValidator"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorCode == "NotEmptyValidator"));
+        }
+
+        #endregion Property name
+
+        [TestMethod]
+        public void CreateUSerWithEmailLenghtGreaterThan50_ShouldReturnsError()
+        {
+            var user = new User("Test", $"{new string('C', 45)}@tt.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 1);
+            Assert.AreEqual("User's email must have less than 50 characters", result.Errors.First().ErrorMessage);
+            Assert.AreEqual("MaximumLengthValidator", result.Errors.First().ErrorCode);
+        }
+
+        [TestMethod]
+        public void CreateUSerWithEmailLenghtLessThan2_ShouldReturnsError()
+        {
+            var user = new User("Test", $"{new string('C', 2)}@tt.com", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 1);
+            Assert.AreEqual("User's email must have more than 10 characters", result.Errors.First().ErrorMessage);
+            Assert.AreEqual("MinimumLengthValidator", result.Errors.First().ErrorCode);
+        }
+
+        [TestMethod]
+        public void CreateUSerWithoutEmail_ShouldReturnsError()
+        {
+            var user = new User("Test", null, "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 2);
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorMessage == "User's email can not be null"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorMessage == "User's email can not be empty"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorCode == "NotNullValidator"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorCode == "NotEmptyValidator"));
+        }
+
+        [TestMethod]
+        public void CreateUSerWithInvalidFormatEmail_ShouldReturnsError()
+        {
+            var user = new User("Test", "teste.com.br", "myPassword2020");
+            var validator = new UserValidator();
+
+            var result = validator.Validate(user);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Count == 1);
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorMessage == "User's email must have a valid format"));
+            Assert.IsTrue(result.Errors.Any(e => e.ErrorCode == "EmailValidator"));
+        }
+    }
+}
