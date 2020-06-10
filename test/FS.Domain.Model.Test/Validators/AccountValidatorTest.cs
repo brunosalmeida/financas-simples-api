c16fa94a370e28e53a8bcbb30636e54c1d7dff62
Bruno Almeida <brunosalmeida@outlook.com>
2020-06-10 13:00:55 +0100

    Add Validation for Account Domain and Expense Domain
    Add Validation for Account Domain and Expense Domain

---
+++ test/FS.Domain.Model.Test/Validators/AccountValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using FS.Domain.Model.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FS.Domain.Model.Test.Validators
+{
+    [TestClass]
+    public class AccountValidatorTest
+    {
+        [TestMethod]
+        public void CreateAccountWithValidData_ShouldReturnsNewAccountInstance()
+        {
+            var user = new User("TestName", "email@email.com", "myPassword2020");
+            var account = new Account(user);
+            var validator = new AccountValidator();
+
+            var result = validator.Validate(account);
+
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
