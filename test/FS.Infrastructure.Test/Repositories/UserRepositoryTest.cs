d5f40f0d6199626f8f3ff1e429300e04f1a7c156
Bruno Almeida <brunosalmeida@outlook.com>
2020-06-14 21:56:37 +0100

    Add tests for insert, update, get and delete methods

--- test/FS.Infrastructure.Test/Repositories/UserRepositoryTest.cs
+++ test/FS.Infrastructure.Test/Repositories/UserRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using FizzWare.NBuilder;
 using FS.Infrastructure.Mappings;
 using FS.Infrastructure.Repositories;
+using FS.Infrastructure.Test.Setup;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MockQueryable.Moq;
@@ -16,53 +17,102 @@ namespace FS.Infrastructure.Test.Repositories
     [TestClass]
     public class UserRepositoryTest
     {
-        private Mock<DatabaseContext> contextMock;
-        private Mock<DbSet<User>> dbSetUserMock;
-        private IEnumerable<User> entities;
+        private Mock<DatabaseContext> _contextMock;
+        private Mock<DbSet<User>> _dbSetUserMock;
+        private IList<User> _entities;
 
         [TestInitialize]
         public void Initialize()
         {
-            entities = Builder<User>
+            _entities = Builder<User>
                 .CreateListOfSize(5)
                 .All()
                 .With(u => u.Id = Guid.NewGuid())
                 .With(u => u.CreatedOn = DateTime.UtcNow)
                 .Build();
 
-            this.dbSetUserMock = entities.AsQueryable().BuildMockDbSet();
-
-            this.contextMock = new Mock<DatabaseContext>();
-            this.contextMock.Setup(c => c.Users).Returns(this.dbSetUserMock.Object);
+            this._dbSetUserMock = _entities.AsQueryable().BuildMockDbSet();
+            this._contextMock = new Mock<DatabaseContext>();
+            this._contextMock.Setup(c => c.Users).Returns(this._dbSetUserMock.Object);
         }
 
         [TestMethod]
-        public void InsertUserWithValidData_ShouldReturns()
+        public async Task InsertUserWithValidData_ShouldReturns()
         {
+            IEnumerable<User> entities = Builder<User>
+               .CreateListOfSize(5)
+               .All()
+               .With(u => u.Id = Guid.NewGuid())
+               .With(u => u.CreatedOn = DateTime.UtcNow)
+               .Build();
+
+
             var model = Builder<Domain.Model.User>
                 .CreateNew()
                 .WithFactory(() => new Domain.Model.User("Test", "email@email.com", "Password123"))
                 .Build();
 
-            this.contextMock.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
+            var setUserMock = MockDbSetFactory.Create<User>(entities);
+            
+
+            var contextMock = new Mock<DatabaseContext>();
+            contextMock.Setup(c => c.Users).Returns(setUserMock.Object);
+            contextMock.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
 
             var repository = new UserRepository(contextMock.Object);
-            Task.Run(() => repository.Insert(model));
+            await repository.Insert(model);
 
-            this.dbSetUserMock.Verify(u => u.Add(It.IsAny<User>()), Times.Once);
-            this.contextMock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+            
+            contextMock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [TestMethod]
         public async Task GetUserWithValidId_ShouldReturns()
         {
-            var id = entities.ElementAt(0).Id;
-            var expected = UserEntityToUserDomainMapper.MapFrom(entities.ElementAt(0));
+            var id = _entities.ElementAt(0).Id;
+            var expected = UserEntityToUserDomainMapper.MapFrom(_entities.ElementAt(0));
 
-            var repository = new UserRepository(contextMock.Object);
+            var repository = new UserRepository(_contextMock.Object);
             var result = await repository.Get(id);
 
             Assert.AreEqual(expected.Id, result.Id);
+        }
+
+        [TestMethod]
+        public async Task UpdateUserWithValidId_ShouldReturns()
+        {
+            var entity = _entities.ElementAt(0);
+
+            var model = UserEntityToUserDomainMapper.MapFrom(entity);
+            model.SetName("Updated Test Name");
+
+            var repository = new UserRepository(_contextMock.Object);
+            await repository.Update(model.Id, model);
+
+            var result = await repository.Get(model.Id);
+
+            Assert.AreEqual(entity.Id, result.Id);
+            Assert.AreEqual("Updated Test Name", result.Name);
+
+            this._contextMock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task DeleteUserWithValidId_ShouldReturns()
+        {
+            var entity = _entities.ElementAt(0);
+
+            this._dbSetUserMock.Setup(u => u.Remove(It.IsAny<User>())).Callback<User>((e) => _entities.Remove(e));
+
+            var repository = new UserRepository(_contextMock.Object);
+            await repository.Delete(entity.Id);
+
+            var result = await repository.Get(entity.Id);
+
+            Assert.IsNull(result);
+
+            this._contextMock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+            this._dbSetUserMock.Verify(u => u.Remove(It.IsAny<User>()), Times.Once);
         }
     }
 }
