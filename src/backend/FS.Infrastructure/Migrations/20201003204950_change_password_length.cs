2d9eeec6ad1ff9d22fe683127c8863ac6686ddc1
Bruno Almeida <brunosalmeida@outlook.com>
2020-10-04 15:16:05 +0100

    Migrations

---
+++ src/backend/FS.Infrastructure/Migrations/20201003204950_change_password_length.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FS.Data.Migrations
+{
+    public partial class change_password_length : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+        }
+    }
+}
